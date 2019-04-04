a4265c94b1a489d950bc34a5d210f95af13daefb
Benjamin Solum <ben.solum@vml.com>
2019-04-04 09:00:15 -0500

    Author base

---
+++ ui.apps/src/main/content/jcr_root/apps/wknd/clientlibs/src/author/author.js
@@ -0,0 +1,23 @@
+import './author.scss';
+
+// Load: this does NOT include .jsx files as components should be included via other files
+let req = require.context('Components', true, /\.author\.js$/);
+req.keys().forEach(req);
+
+// Development HMR
+if (process.env.NODE_ENV === 'development') {
+  if (module.hot) {
+    module.hot.accept(req.id, () => {
+      //Clean jQuery event delegations in content frame
+      try {
+        $(document).off();
+        $(window).off();
+      } catch {
+        //
+      }
+
+      req = require.context('Components', true, /\.author\.js$/);
+      req.keys().forEach(req);
+    });
+  }
+}
