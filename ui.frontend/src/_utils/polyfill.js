f8121bf5db03067bd662ac4e5a7504702465f1df
Benjamin Solum <ben.solum@vml.com>
2019-04-17 14:33:47 -0500

    Remove polyfill setup.  Assume >IE11

--- ui.frontend/src/_utils/polyfill.js
+++ ui.frontend/src/_utils/polyfill.js
@@ -1,12 +1,13 @@
-export default function() {
-  const polyfills = [];
+// Enable if supporting IE11 or provide alternative
+// import 'es6-promise/auto';
 
-  // URLSearchParams (https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams)
-  if (!Modernizr.urlsearchparams) {
-    polyfills.push(
-      import(/* webpackChunkName: "site/resources/polyfill-urlsearchparams" */ 'url-search-params-polyfill')
-    );
-  }
+const polyfills = [];
 
-  return Promise.all(polyfills);
+// URLSearchParams (https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams)
+if (!Modernizr.urlsearchparams) {
+  polyfills.push(
+    import(/* webpackChunkName: "site/resources/polyfill-urlsearchparams" */ 'url-search-params-polyfill')
+  );
 }
+
+export default Promise.all(polyfills);
--- ui.frontend/src/site/site.js
+++ ui.frontend/src/site/site.js
@@ -1,32 +1,27 @@
-import promise from 'Utils/promise';
 import polyfills from 'Utils/polyfill';
 import './grid.less';
 import './site.scss';
 
-function setup() {
-  polyfills().then(() => {
-    // Load: this does NOT include .jsx files as components should be included via other files
-    let req = require.context('Components', true, /\.site\.js$/);
-    req.keys().forEach(req);
+polyfills.then(() => {
+  // Load: this does NOT include .jsx files as components should be included via other files
+  let req = require.context('Components', true, /\.site\.js$/);
+  req.keys().forEach(req);
 
-    // Development HMR
-    if (process.env.NODE_ENV === 'development') {
-      if (module.hot) {
-        module.hot.accept(req.id, () => {
-          //Clean jQuery event delegations in content frame
-          try {
-            $(document).off();
-            $(window).off();
-          } catch {
-            //
-          }
+  // Development HMR
+  if (process.env.NODE_ENV === 'development') {
+    if (module.hot) {
+      module.hot.accept(req.id, () => {
+        //Clean jQuery event delegations in content frame
+        try {
+          $(document).off();
+          $(window).off();
+        } catch {
+          //
+        }
 
-          req = require.context('Components', true, /\.site\.js$/);
-          req.keys().forEach(req);
-        });
-      }
+        req = require.context('Components', true, /\.site\.js$/);
+        req.keys().forEach(req);
+      });
     }
-  });
-}
-
-Modernizr.promises ? setup() : promise(setup);
+  }
+});
